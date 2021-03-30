4c292aeb1bf55fb2f91f572c276743cd376ad6f7
HumpyMiryala <72124335+HumpyMiryala@users.noreply.github.com>
2021-03-31 00:43:45 +0530

    Add files via upload

---
+++ src/app/Untitled-2.py
@@ -0,0 +1,3 @@
+dict 1={
+dict 1["x"]=[5]
+print (dict 1["x"][5])
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+
+const MaterialComponents=[
+  
+]
+
+
+@NgModule({
+  
+  imports: [],
+  exports: []
+})
+export class MaterialModule { }
